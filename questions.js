fe4619e4fe6357c83788ef6faf3753dd05341a32
ramram27 <042rohit@gmail.com>
2021-07-29 08:54:27 +0200

    My quiz application

---
+++ questions.js
@@ -0,0 +1,121 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of the following kinds of derivation is used by LR parsers?",
+    answer: "Rightmost in reverse",
+    options: [
+      " Leftmost",
+      "Leftmost in reverse",
+      " Rightmost",
+      "Rightmost in reverse"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of the following is NOT a valid identity? ",
+    answer: "(x + y) ⊕ z = x ⊕ (y + z)",
+    options: [
+      "(x ⊕ y) ⊕ z = x ⊕ (y ⊕ z)",
+       "(x + y) ⊕ z = x ⊕ (y + z)",
+      "x ⊕ y = x + y, if xy = 0",
+      "x ⊕ y = (xy + x′y′)′"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The value of 351 mod 5 is _________ . Note: This was Numerical Type question.",
+    answer: "2",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+     {
+     numb: 6,
+    question: "Which one of the following are the appropriate choices for X1, X2, X3 and X4?",
+     answer: "X1 = L, X2 = T, X3 = L1, X4 = L",
+     options: [
+       "X1 = L, X2 = T, X3 = L1, X4 = L",
+       "X1 = L, X2 = L, X3 = L1, X4 = T",
+      " X1 = T, X2 = L, X3 = L1, X4 = T",
+     "X1 = T, X2 = L, X3 = T, X4 = L1"
+    ]
+  },
+
+  {
+    numb:7,
+    question: "Which type of JavaScript language is ___.",
+    answer: "Object-Based",
+    options: [
+         "Object-Oriented",
+         "Object-Based",
+         "Assembly-language",
+         "High-level"
+
+    ]
+  },
+
+  {
+    numb:8,
+    question:"Which one of the following also known as Conditional Expression:",
+    answer:"immediate if",
+    options:[
+      "If-then-else statement",
+      "Alternative to if-else",
+      "immediate if",
+      "Switch statement"
+    ]
+  },
+
+  {
+    numb:9,
+    question:"When interpreter encounters an empty statements, what it will do:",
+    answer:"Ignores the statements",
+    options:[
+      "Ignores the statements",
+      "Throws an error",
+      "Prompts to complete the statement",
+      "Shows a warning"
+    ]
+  },
+
+  {
+  numb:10,
+  question:'The "function" and " var" are known as:',
+  answer:"Declaration statements",
+  options:[
+    "Data types",
+    "Declaration statements",
+    "Keywords",
+    "Prototypes"
+  ]
+
+  }
+
+];
